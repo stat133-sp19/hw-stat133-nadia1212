d88d6c0b9112046851b2d7aaef85cb2ac92ba0eb
Nadia Aquil <nadia@berkeley.edu>
2019-03-13 16:46:13 -0700

    update shot charts

--- workout01/code/make-shots-charts-script.R
+++ workout01/code/make-shots-charts-script.R
@@ -8,7 +8,7 @@
 library(grid)
 library(jpeg)
 library(ggplot2)
-
+library(dplyr)
 
 shots_data <- read.csv("../workout01/data/shots-data.csv", stringsAsFactors = FALSE)
 
@@ -17,13 +17,72 @@ court_image <- rasterGrob(
   readJPEG(court_file),
   width = unit(1, "npc"),
   height = unit(1, "npc"))
-str(shots_data)
-klay_scatterplot <- ggplot(data = shots_data[shots_data$name == "Klay Thompson"]) +
+
+
+dat <- shots_data[shots_data$name == "Andre Iguodala", ]
+str(dat)
+pdf(file = "../workout01/images/andre-iguodala-shot-chart.pdf", width = 6.5, height = 5)
+iguodala_scatterplot <- ggplot(data = dat) +
+  annotation_custom(court_image, -250, 250, -50, 420) + 
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) + 
+  ggtitle('Shot Chart: Andre Iguodala (2016 season)') +
+  theme_minimal()
+print(iguodala_scatterplot)
+dev.off()
+
+dat <- shots_data[shots_data$name == "Draymond Green", ]
+str(dat)
+pdf(file = "../workout01/images/draymond-green-shot-chart.pdf", width = 6.5, height = 5)
+green_scatterplot <- ggplot(data = dat) +
+  annotation_custom(court_image, -250, 250, -50, 420) + 
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) + 
+  ggtitle('Shot Chart: Draymond Green (2016 season)') +
+  theme_minimal()
+print(green_scatterplot)
+dev.off()
+
+
+dat <- shots_data[shots_data$name == "Kevin Durant", ]
+str(dat)
+pdf(file = "../workout01/images/kevin-durant-shot-chart.pdf", width = 6.5, height = 5)
+durant_scatterplot <- ggplot(data = dat) +
+  annotation_custom(court_image, -250, 250, -50, 420) + 
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) + 
+  ggtitle('Shot Chart: Kevin Durant (2016 season)') +
+  theme_minimal()
+print(durant_scatterplot)
+dev.off()
+
+dat <- shots_data[shots_data$name == "Klay Thompson", ]
+str(dat)
+pdf(file = "../workout01/images/klay-thompson-shot-chart.pdf", width = 6.5, height = 5)
+thompson_scatterplot <- ggplot(data = dat) +
+  annotation_custom(court_image, -250, 250, -50, 420) + 
   geom_point(aes(x = x, y = y, color = shot_made_flag)) +
   ylim(-50, 420) + 
   ggtitle('Shot Chart: Klay Thompson (2016 season)') +
   theme_minimal()
+print(thompson_scatterplot)
+dev.off()
+
+dat <- shots_data[shots_data$name == "Stephen Curry", ]
+str(dat)
+pdf(file = "../workout01/images/stephen-curry-shot-chart.pdf", width = 8, height = 7)
+curry_scatterplot <- ggplot(data = dat) +
+  annotation_custom(court_image, -250, 250, -50, 420) + 
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) + 
+  ggtitle('Shot Chart: Stephen Curry (2016 season)') +
+  theme_minimal()
+print(curry_scatterplot)
+dev.off()
 
 
-
-
+dat <- shots_data
+pdf(file = "../workout01/images/gsw-shot-charts.pdf", width = 6.5, height = 5)
+print(facet_wrap_paginate(iguodala_scatterplot, green_scatterplot))
+      #durant_scatterplot, thompson_scatterplot, curry_scatterplot))
+dev.off()
---
+++ workout01/data-dictionary.md
@@ -0,0 +1,13 @@
+team_name: Name of team 
+game_date: Date of game
+season,
+period,
+minutes_remaining,
+seconds_remaining,
+shot_made_flag,
+action_type
+shot_type,
+shot_distance,
+opponent,
+x: x coordinate of player's position when making shot 
+y: y coordinate of player's position when making shot
